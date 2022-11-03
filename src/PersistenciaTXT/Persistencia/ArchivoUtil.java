84601b42f847afb0a605765b9b942c5fda42fc77
PruebasPA <jandpos@cqteduca.edu.com>
2022-11-02 20:47:42 -0500

    Finish

---
+++ src/PersistenciaTXT/Persistencia/ArchivoUtil.java
@@ -0,0 +1,31 @@
+package PersistenciaTXT.Persistencia;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class ArchivoUtil {
+    public static ArrayList<String> leerArchivo(String ruta) throws IOException {
+
+    ArrayList<String> contenido = new ArrayList<String>();
+    FileReader fr = new FileReader(ruta);
+    BufferedReader bfr = new BufferedReader(fr);
+    String linea = "";
+    while((linea = bfr.readLine())!= null) {
+        contenido.add(linea);
+    }
+    bfr.close();
+    fr.close();
+    return contenido;
+}
+
+    public static void guardarArchivo(String ruta, String contenido, boolean b) throws IOException {
+
+        FileWriter fw = new FileWriter(ruta, b);
+        BufferedWriter bfw = new BufferedWriter(fw);
+        bfw.write(contenido);
+        bfw.close();
+        fw.close();
+
+    }
+
+}
---
+++ src/PersistenciaTXT/Persistencia/Model/a.java
@@ -0,0 +1,42 @@
+package PersistenciaTXT.Persistencia.Model;
+
+public class a {
+private String name;
+private String age;
+private String friend;
+
+
+public a(){
+super();
+//  TODO Auto-generated constructor stub
+}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getFriend() {
+        return friend;
+    }
+
+    public void setFriend(String friend) {
+        this.friend = friend;
+    }
+    public a(String name, String age, String friend) {
+        this.name = name;
+        this.age = age;
+        this.friend = friend;
+    }
+
+}
